56b8dd5ba8cb4756ce597178471bc747f97b956c
Laura Viglioni <laura.viglioni@idwall.co>
2020-04-13 12:27:12 -0300

    idwall

---
+++ elpa/websocket-20200102.637/websocket-autoloads.el
@@ -0,0 +1,15 @@
+;;; websocket-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
+
+;;;### (autoloads nil nil ("websocket.el") (24166 21583 453340 278000))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; websocket-autoloads.el ends here
